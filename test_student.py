8cd00c0b5715b325dee44ead9ddea93d40108c6e
alicia18-meet <aliciakk2@gmail.com>
2016-12-21 08:13:08 +0200

    Update test_student.py

--- test_student.py
+++ test_student.py
@@ -3,5 +3,5 @@ from Student import student
 alicia=student('Alicia', 'Jerusalem', '16', '1.6', 'chocolate')
 ##student.print_summary(alicia)
 
-tamir=student('Tamir', 'Jerusalem', '15.5', '1.7', 'penis')
+tamir=student('Tamir', 'Jerusalem', '15.5', '1.7', 'Bananas')
 student.print_summary(tamir)
